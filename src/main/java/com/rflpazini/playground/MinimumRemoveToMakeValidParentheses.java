f393d4331f402f7e8e8b0ce27df9e7ff1ce8bbe7
Rafael Pazini <rflpazini@gmail.com>
2022-02-01 22:18:03 -0300

    add: remove parentheses to make it valid

---
+++ src/main/java/com/rflpazini/playground/MinimumRemoveToMakeValidParentheses.java
@@ -0,0 +1,40 @@
+package com.rflpazini.playground;
+
+public class MinimumRemoveToMakeValidParentheses {
+
+  public static void main(String[] args) {
+    minRemoveToMakeValid("lee(t(c)o)de)"); // lee(t(c)o)de
+    minRemoveToMakeValid("))(("); // ""
+  }
+
+  public static String minRemoveToMakeValid(String s) {
+    int count = 0;
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+
+      // Check if is a letter
+      if (c != '(' && c != ')') {
+        sb.append(c);
+      } else {
+        // Increase parentheses' count
+        if (c == '(') {
+          count++;
+          sb.append(c);
+        } else if (count > 0) {
+          sb.append(c);
+          count--;
+        }
+      }
+    }
+
+    // Remove reminding parentheses
+    while (count > 0) {
+      sb.deleteCharAt(sb.lastIndexOf("("));
+      count--;
+    }
+
+    return sb.toString();
+  }
+}
